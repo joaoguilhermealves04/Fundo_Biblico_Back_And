53fd2aae91cf1e410829ba22085323a624b5c146
joaog <joaog@LAPTOP-7251LD7D>
2024-04-13 16:18:45 -0300

    Limpeza no codigo

--- FundoBiblico.Aplication/Servicos/ProdutoServicos.cs
+++ FundoBiblico.Aplication/Servicos/ProdutoServicos.cs
@@ -3,12 +3,6 @@ using FundoBiblico.Aplication.IServicos;
 using FundoBiblico.Aplication.Models;
 using FundoBiblico.Dominio.Entity;
 using FundoBiblico.Dominio.Interfaces;
-using FundoBiblico.Repository.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FundoBiblico.Aplication.Servicos
 {
